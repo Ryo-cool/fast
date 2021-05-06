54734d4e6d8cd21f2d2c61f64e681520d74a8c18
Ryo_Onodera <coolboxing576@gmail.com>
2021-05-06 21:11:05 +0900

    TailwindCss

--- front/app/pages/index.tsx
+++ front/app/pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import { GetStaticProps } from "next";
+import Header from './components/Header'
 
 type Post = {
   id: number;
@@ -13,15 +14,44 @@ type Props = {
 const Home: FC<Props> = (props) => {
   return (
     <div>
-      <h2>POSTの一覧</h2>
-      <table>
-	{props.posts.map((post) =>
-	  <tr>
-	    <td>{post.id}.</td>
-	    <td>{post.title}</td>
-	  </tr>
-        )}
-      </table>
+      <Header />
+      <div className="h-screen w-4/5 max-w-5xl mx-auto flex items-center justify-center flex-col">
+        <h2 className="my-3 hover:text-blue-300">POSTの一覧</h2>
+        <table>
+	    {props.posts.map((post) =>
+	    <tr>
+	      <td>{post.id}.</td>
+	      <td>{post.title}</td>
+	    </tr>
+          )}
+        </table>
+        <div className="bg-gray-50">
+          <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              <span className="block">飛び込む準備はできましたか？</span>
+              <span className="block text-indigo-600">今日から無料トライアルを始めましょう。</span>
+            </h2>
+            <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+              <div className="inline-flex rounded-md shadow">
+                <a
+                  href="#"
+                  className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+                >
+                  Get started
+                </a>
+              </div>
+              <div className="ml-3 inline-flex rounded-md shadow">
+                <a
+                  href="#"
+                  className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
